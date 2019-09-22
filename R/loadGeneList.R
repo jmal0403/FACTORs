6246533f979ed0db9ec4922436f5a73d165cbe64
John Malamon <johnmalamon@Malamon-MBP.hsd1.nj.comcast.net>
2019-09-21 21:53:14 -0400

    first

---
+++ R/loadGeneList.R
@@ -0,0 +1,12 @@
+#' loadGeneList function
+#'
+#' Load tab delimited file of functional associations of gene a gene set .
+#' @param file.IN tab delimited file with three columns -  1) gene identifiers, 2) t-statistic of functional association 1, and 3) t-statistic for gene functional association 2.
+#' @return data.frame 
+#' @export 
+#' @examples
+#' loadGeneList()
+loadGeneList <- function(file.IN=test.file.01){
+read.csv(file.IN, header = TRUE, sep="\t")
+}
+
