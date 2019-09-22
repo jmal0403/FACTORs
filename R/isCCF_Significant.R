5b15cbd6a0208c46c98f13b877333a3f4b90c478
John Malamon <johnmalamon@Malamon-MBP.hsd1.nj.comcast.net>
2019-09-22 15:02:09 -0400

    fourth

--- R/isCCF_Significant.R
+++ R/isCCF_Significant.R
@@ -6,7 +6,7 @@
 #' @export 
 #' @examples
 #' isCCF_Significant()
-isCCF_Sigificant<-function(df=ccf.df){
+isCCF_Significant<-function(df=ccf.df){
 apply(ccf.df.01$acf, MARGIN = c(1,2), function(x) x >= ccf.df.01$acf[[1]])
 }
 
